8ccd705ddb72e107cf8f8b12dc22f3259ad943df
Christoph Dwertmann <cdwertmann@gmail.com>
2015-02-06 15:38:15 +1100

    receives mail successfully

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,6 @@ RUN apt-get update && apt-get -y install supervisor postfix
 
 # Add files
 ADD assets/install.sh /opt/install.sh
-ADD assets/filter.sh /usr/local/bin/filter.sh
 
 # Run
 CMD /opt/install.sh;/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
--- assets/install.sh
+++ assets/install.sh
@@ -34,7 +34,8 @@ postconf -e 'smtpd_banner=$myhostname Microsoft ESMTP MAIL Service, Version: 5.0
 
 postconf -e virtual_alias_domains=$mydestination
 postconf -e virtual_alias_maps=hash:/etc/postfix/virtual
-postconf -X mydestination
+postconf -e 'smtpd_relay_restrictions=permit_mynetworks reject_unauth_destination'
+postconf -e mydestination=localhost
 
 # disable sending
 postconf -e default_transport=error
@@ -45,6 +46,7 @@ cat >> /etc/postfix/virtual <<EOF
 @$mydestination mail@localhost
 EOF
 postmap /etc/postfix/virtual
+chown mail. -R /var/mail
 
 ############
 # Enable TLS
@@ -54,22 +56,9 @@ if [[ -n "$(find /etc/postfix/certs -iname *.crt)" && -n "$(find /etc/postfix/ce
   postconf -e smtpd_tls_cert_file=$(find /etc/postfix/certs -iname *.crt)
   postconf -e smtpd_tls_key_file=$(find /etc/postfix/certs -iname *.key)
   chmod 400 /etc/postfix/certs/*.*
-  # /etc/postfix/master.cf
-  postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd"
-  postconf -P "submission/inet/syslog_name=postfix/submission"
-  postconf -P "submission/inet/smtpd_tls_security_level=encrypt"
-  postconf -P "submission/inet/smtpd_sasl_auth_enable=yes"
-  postconf -P "submission/inet/milter_macro_daemon_name=ORIGINATING"
-  postconf -P "submission/inet/smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination"
-  postconf -P "submission/inet/content_filter=protective_markings:dummy"
   postconf -e smtpd_tls_ciphers=high
   postconf -e smtpd_tls_exclude_ciphers=aNULL,MD5
   postconf -e smtpd_tls_security_level=may
   # Preferred syntax with Postfix ≥ 2.5:
   postconf -e smtpd_tls_protocols=!SSLv2,!SSLv3
 fi
-# client TLS
-postconf -e smtp_tls_security_level=may
-postconf -e smtp_tls_ciphers=high
-postconf -e smtp_tls_exclude_ciphers=aNULL,MD5
-postconf -e smtp_tls_protocols=!SSLv2,!SSLv3
